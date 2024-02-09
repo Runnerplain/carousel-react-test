a3de4ec5738e441ed53681fc8e6cef84599184f1
orten <gazel-edouard@protonmail.com>
2024-02-09 14:57:49 +0100

    focusCard WIP

--- src/components/IKProjectFocusCard/IKProjectFocusCard.jsx
+++ src/components/IKProjectFocusCard/IKProjectFocusCard.jsx
@@ -1,10 +1,20 @@
 import './IKProjectFocusCard.css'
-
+import picture2 from '../../assets/pictures/picture2.jpg'
 
 export default function IKProjectFocusCard() {
   return (
     <div className='focusCardContainer'>
-            focus
+      <div className="focusBox">
+        <img className='pictureFocus' src={picture2} alt="" />
+        <div className="__overlay">
+          <h3 className='titleProjectFocusCard'>Projet 1</h3>
+          <p className='tagProjectFocusCard'>#tag1</p>
+          <p className='descriptionFocusCard'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima beatae quae distinctio aliquid odit provident praesentium, eum quisquam ab dolore!</p>
+          <p className='techUsingFocusCard'>Tecnos utilisées</p>
+          <div className="logoTech"></div>
+          <button>Découvrir le projet</button>
+          </div>
+      </div>
     </div>
   )
 }
--- src/components/IKProjectMiniCard/IKProjectMiniCard.jsx
+++ src/components/IKProjectMiniCard/IKProjectMiniCard.jsx
@@ -5,8 +5,8 @@ import picture1 from '../../assets/pictures/picture1.jpg';
 export default function IKProjectMiniCard() {
   return (
     <div className='pictureContainer'>
-      <img src={picture1} alt="" />
-      <p>Lorem</p>
+      <img className='pictureMini' src={picture1} alt="" />
+      <p className='titlePicture'>Lorem</p>
     </div>
   )
 }
